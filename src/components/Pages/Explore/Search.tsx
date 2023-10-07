193e0db98f650991863f648f7910fb221543ae8d
UTSAB PANDEY <pandeyutsab0@gmail.com>
2023-10-07 15:26:58 +0545

    explore add

--- src/components/Pages/Explore/Search.tsx
+++ src/components/Pages/Explore/Search.tsx
@@ -7,12 +7,12 @@ const Search = () => {
             <form className=" flex md:flex-row gap-3">
                 <div className="flex ">
                     <input type="text" placeholder="Search for projects"
-                        className="  w-[60vw] px-3 h-10 rounded-l border-2 border-sky-500 focus:outline-none focus:border-sky-500"
+                        className="  w-[50vw] px-3 h-10 rounded-l border-2 border-sky-500 focus:outline-none focus:border-sky-500"
                     />
                     <button type="submit" className="bg-sky-500 text-white rounded-r px-2 md:px-3 py-0 md:py-1">Search</button>
                 </div>
                 <select id="pricingType" name="pricingType"
-                    className=" h-10 border-2 border-sky-500 focus:outline-none focus:border-sky-500 text-sky-500 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider">
+                    className=" w-[20vw] px-3 h-10 border-2 border-sky-500 focus:outline-none focus:border-sky-500 text-sky-500 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider">
                     <option value="All" selected="">All</option>
                     <option value="Openscience">open science</option>
                     <option value="Opensource">open source</option>
@@ -21,7 +21,7 @@ const Search = () => {
                     <option value="expert">professional</option>
                 </select>
                 <select id="pricingType" name="pricingType"
-                    className=" h-10 border-2 border-sky-500 focus:outline-none focus:border-sky-500 text-sky-500 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider">
+                    className=" w-[20vw] px-3 h-10 border-2 border-sky-500 focus:outline-none focus:border-sky-500 text-sky-500 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider">
                     <option value="All" selected="">All</option>
                     <option value="Openscience">open science</option>
                     <option value="Opensource">open source</option>
