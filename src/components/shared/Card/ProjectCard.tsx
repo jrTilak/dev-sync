d6f2452fbe21160aeb4e2304063b6256efb19424
jrTilak <tilakthapa9898@gmail.com>
2023-10-08 15:09:30 +0545

    convert a to link

--- src/components/shared/Card/ProjectCard.tsx
+++ src/components/shared/Card/ProjectCard.tsx
@@ -60,7 +60,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
             </a>
           </p>
           <p className="mt-4 text-base text-gray-600">{metaDesc}</p>
-          <a
+          <Link
             href={`/projects/${projectId}`}
             title=""
             className="inline-flex items-center justify-center pb-0.5 mt-5 text-base font-semibold text-blue-600 transition-all duration-200 border-b-2 border-transparent hover:border-blue-600 focus:border-blue-600"
@@ -78,7 +78,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
                 clipRule="evenodd"
               />
             </svg>
-          </a>
+          </Link>
         </div>
       </div>
     </>
